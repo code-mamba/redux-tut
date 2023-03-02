895874a7ff16b90427e6e89b723177ed795fb865
code-mamba <dhanushbaskaran2000@gmail.com>
2023-03-02 09:54:42 +0530

    redux-updated

--- redux-tut/src/App.js
+++ redux-tut/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import ReduxCounter from './component/reduxCounter';
-import { useSelector, useDispatch } from 'react-redux';
+
 function App() {
   
   return (
